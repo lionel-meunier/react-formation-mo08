b0d776dc82a25706df57b7ba1068ba5c259c2560
Lionel MEUNIER <lmeunier@iocean.fr>
2020-12-18 11:46:39 +0100

    Fix correction exo 2

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,10 +1,36 @@
 import { Component } from "react";
 
 export class Home extends Component {
+  movies = [
+    {
+      title: "Coup de foudre à Notting Hill",
+      note: 5,
+    },
+    {
+      title: "Piège de cristal",
+    },
+    {
+      title: "Rasta Rocket",
+    },
+    {
+      title: "Le Plus beau des combats",
+    },
+    {
+      title: "La Cité de la peur",
+    },
+  ];
+
   render() {
+    const movies = this.movies.map((m, i) => (
+      <li key={i}>
+        {m.title}
+        {m.note && <span class="badge badge-primary ml-2">{m.note}</span>}
+      </li>
+    ));
     return (
       <div>
         <h1>My home</h1>
+        <ul>{movies}</ul>
       </div>
     );
   }
