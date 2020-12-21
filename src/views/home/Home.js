621f20c7b908c91a98e98cec1915c1598dc9bbde
Lionel MEUNIER <lmeunier@iocean.fr>
2020-12-21 08:02:51 +0100

    Correction TP 5

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -5,7 +5,7 @@ export class Home extends Component {
   movies = [
     {
       title: "Coup de foudre à Notting Hill",
-      note: 5,
+      note: 3,
     },
     {
       title: "Piège de cristal",
@@ -18,6 +18,7 @@ export class Home extends Component {
     },
     {
       title: "La Cité de la peur",
+      note: 5,
     },
   ];
 
@@ -26,30 +27,40 @@ export class Home extends Component {
     this.state = { movies: this.movies };
   }
 
-  changeNote(newNote, indexMovie) {
+  changeNote(newNote, movie) {
     this.setState((prevState) => {
-      prevState.movies[indexMovie].note = newNote;
+      const index = prevState.movies.indexOf(movie);
+      prevState.movies[index].note = newNote;
       return prevState;
     });
   }
 
   render() {
-    const movies = this.state.movies.map((m, i) => {
-      let note;
-      if (m.note) {
-        note = <span className="badge badge-primary ml-2">{m.note}</span>;
-      }
-      return (
-        <li key={i}>
-          {m.title}
-          {note}
-          <Note
-            currentNote={m.note}
-            onChangeNote={(newNote) => this.changeNote(newNote, i)}
-          ></Note>
-        </li>
-      );
-    });
+    const movies = this.state.movies
+      .sort((a, b) => {
+        if (!a.note) {
+          return 1;
+        } else if (!b.note) {
+          return -1;
+        }
+        return b.note - a.note;
+      })
+      .map((m, i) => {
+        let note;
+        if (m.note) {
+          note = <span className="badge badge-primary ml-2">{m.note}</span>;
+        }
+        return (
+          <li key={i}>
+            {m.title}
+            {note}
+            <Note
+              currentNote={m.note}
+              onChangeNote={(newNote) => this.changeNote(newNote, m)}
+            ></Note>
+          </li>
+        );
+      });
     return (
       <Fragment>
         <h1>My home</h1>
